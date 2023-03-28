326ee48d05e6847cd0b3de228078a37cd76e2c74
pramadeish <111697757+pramadeish@users.noreply.github.com>
2023-03-28 20:11:05 +0530

    added role based jwt(verifying user or admin)

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,29 @@
 import express from "express";
+import { deleteUser, getAllUser, getUser, updateUser } from "../controllers/userController.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 
 const router = express.Router();
+
+// router.get("/checkauthentication",verifyToken,(req,res,next)=>{
+//     res.send("hello user you are loggen in!")
+// })
+
+// router.get("/checkuser/:id",verifyUser,(req,res,next)=>{
+//     res.send("hello user you are logged in and you can delete your account")
+// })
+// router.get("/checkadmin/:id",verifyAdmin,(req,res,next)=>{
+//     res.send("hello Admin you are logged in and you can delete all account")
+// })
+
+//UPDATE
+router.put("/:id",verifyUser,updateUser)
+//DELETE
+router.delete("/:id",verifyUser,deleteUser)
+//GET
+router.get("/:id",verifyUser,getUser)
+//GETALL
+router.get("/",verifyAdmin,getAllUser)
+
 
 export default router;
